f4be239a9812faa5b16063881f2ee4691dd167d2
Oleksandr Kovalyk <42751978+Kovaxs@users.noreply.github.com>
2025-03-26 21:30:30 +0100

    feat: no longer need nix-daemon

--- flake.nix
+++ flake.nix
@@ -170,9 +170,6 @@
     system.defaults = {
         dock.autohide = true;
     };
-    # Auto upgrade nix package and the daemon service.
-    services.nix-daemon.enable = true;
-    # nix.package = pkgs.nix;
 
     # Necessary for using flakes on this system.
     nix.settings.experimental-features = "nix-command flakes";
