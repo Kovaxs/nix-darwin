3f7450ea3778d2b7b1f2c1d90cf8eed92ff4b637
Oleksandr Kovalyk <42751978+Kovaxs@users.noreply.github.com>
2025-08-21 17:13:50 +0200

    feat: added pandoc

--- flake.nix
+++ flake.nix
@@ -98,6 +98,7 @@
                         "sioyek"
                     ];
                     brews = [
+                        "pandoc"
                         "libomp"
                         "libmagic"
                         "cmake"
